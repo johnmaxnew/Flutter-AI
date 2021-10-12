34957f01ddf3b7443207c7135e5e5aca4e378e3c
johnmaxnew <forevermeticulous@gmail.com>
2021-10-12 22:40:17 +0300

    modified model

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -17,7 +17,6 @@ class _HomePageState extends State<HomePage> {
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     fetchRadios();
   }
@@ -27,6 +26,10 @@ class _HomePageState extends State<HomePage> {
     radios = MyRadioList.fromJson(radioJson).radios;
     // ignore: avoid_print
     print(radios);
+
+    setState(() {
+      
+    });
   }
 
   @override
